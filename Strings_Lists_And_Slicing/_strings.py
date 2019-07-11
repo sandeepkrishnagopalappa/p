074d33382870831aef0ee24680bb9101576d4108
sandeepsuryaprasad <sandeep.suryaprasad@gmail.com>
2019-07-11 19:47:23 +0530

    added new example for string formatting

--- Strings_Lists_And_Slicing/_strings.py
+++ Strings_Lists_And_Slicing/_strings.py
@@ -105,3 +105,26 @@ addresses = ['5412 N CLARK ST',
         ]
 
 print([add for add in addresses if fnmatch(add, '* ST')])
+
+# String formatting using f strings
+
+fname = 'Steve'
+lname = 'Jobs'
+
+print(f'Hello {fname} {lname} welcome to Python!')
+
+portfolios = [
+    {'name': 'IBM', 'shares': 100, 'price': 32.212},
+    {'name': 'AAPL', 'shares': 60, 'price': 340.323},
+    {'name': 'MSFT', 'shares': 140, 'price': 150.545},
+    {'name': 'FB', 'shares': 80, 'price': 90.345},
+    {'name': 'ACME', 'shares': 35, 'price': 37.908}
+]
+
+print(f'{"name":>8} {"shares":>8} {"price":>8}')    # Write headers
+
+for portfolio in portfolios:
+    name = portfolio.get('name')
+    shares = portfolio.get('shares')
+    price = portfolio.get('price')
+    print(f'{name:>8} {shares:>8} {price:>8.2f}')
