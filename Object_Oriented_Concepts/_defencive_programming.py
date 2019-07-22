a5234843f1232955deecba38872da9c503630edf
sandeepsuryaprasad <sandeep.suryaprasad@gmail.com>
2019-07-22 20:17:19 +0530

    added example for to demonstrate abstract methods

---
+++ Object_Oriented_Concepts/_defencive_programming.py
@@ -0,0 +1,76 @@
+from abc import ABC, abstractmethod
+
+
+class Bank(ABC):
+
+    def __init__(self, amount):
+        self.amount = amount
+
+    @abstractmethod
+    def deposit(self):
+        pass
+
+    @abstractmethod
+    def withdraw(self):
+        pass
+
+    @abstractmethod
+    def statement(self):
+        pass
+
+
+class Citi(Bank):
+    def __init__(self, amount):
+        super().__init__(amount)
+
+    def deposit(self):
+        print('Depositing Amount to Citi Bank')
+
+    def withdraw(self):
+        print('Withdrawing Amount from Citi Bank')
+
+    def statement(self):
+        print('Printing Citi Bank Statement')
+
+
+class Sbi(Bank):
+    def __init__(self, amount):
+        super().__init__(amount)
+
+    def deposit(self):
+        print('Depositing Amount to SBI Bank')
+
+    def withdraw(self):
+        print('Withdrawing Amount from SBI Bank')
+
+    def statement(self):
+        print('Printing SBI Bank Statement')
+
+
+class Hdfc(Bank):
+    def __init__(self, amount):
+        super().__init__(amount)
+
+    def deposit(self):
+        print('Depositing Amount to HDFC Bank')
+
+    def withdraw(self):
+        print('Withdrawing Amount from HDFC Bank')
+
+    def statement(self):
+        print('Printing HDFC Bank Statement')
+
+
+def bank_transactions(bank, transaction_name):
+    if transaction_name.lower() == 'deposit':
+        bank.deposit()
+    elif transaction_name.lower() == 'withdraw':
+        bank.withdraw()
+    else:
+        bank.statement()
+
+
+b = Hdfc(1000)
+bank_transactions(b, 'deposit')
+
+
