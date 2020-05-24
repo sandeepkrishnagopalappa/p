b686c5177bdf9f8f354e7169729a16762a5dbc2a
sandeepsuryaprasad <sandeep.suryaprasad@dxc.com>
2020-05-24 17:48:53 +0530

    list-tuple-set speed test

--- Strings_Lists_And_Slicing/_sets.py
+++ Strings_Lists_And_Slicing/_sets.py
@@ -29,15 +29,74 @@ c = {6, 7, 8, 9}
 a.union(b, c)   # Prints {1, 2, 3, 4, 5, 6, 7, 8, 9}
 
 # Set Intersection
-a.intersection(b)   # Prints {3, 4}
+a.intersection(b)   # Returns a new set {3, 4}
 
 # Set Difference
-# Prints all the elements in set a which are not in b
+# Returns a new set with the elements in set a which are not in b
 a.difference(b)
 # a - b   # Prints {1, 2}
 
 a = {1, 2, 3, 30, 300}
 b = {10, 20, 30, 40}
 c = {100, 200, 300, 400}
-a.difference(b, c)  # Prints {1, 2, 3}
+a.difference(b, c)  # Returns a new set {1, 2, 3}
 # a - b - c
+
+# isdisjoint()
+# Returns True if there are no items in common between two sets
+x = {1, 2, 3}
+y = {2, 3, 5}
+x.isdisjoint(y)     # Returns False
+
+x1 = {1, 3, 5}
+x2 = {2, 4, 6}
+x1.isdisjoint(x2)
+
+# issubset()
+# A set is considered a subset of another set if every element of the first set is
+# in the second
+a = {1}
+b = {1, 2}
+c = {1, 2, 3}
+d = {1, 2, 4}
+
+a.issubset(b)   # Returns True
+b.issubset(c)   # Returns True
+c.issubset(d)   # Returns False
+
+# Modifying a Set add()
+a = {'apple', 'yahoo', 'google'}
+a.add('facebook')
+
+# remove()
+a.remove("apple")
+# Element must exist in the set. Otherwise python throws key Error
+
+# discard()
+# Removes an element from the set.
+# If the element does not exist, Key error is not thrown
+a.discard("apple")
+
+# pop()
+# Removes and returns a random item from the set
+# Throws KeyError if the set is empty
+a.pop()
+
+# clear()
+# removes all the elements from the set
+a.clear()
+
+# update()
+a = {1, 2}
+b = {3, 4}
+a.update(b)     # a = {1, 2, 3, 4}
+
+# intersection_update()
+# Modify a set by retaining only elements found in both sets
+a = {1, 2, 3}
+b = {2, 3, 4}
+a.intersection_update(b)    # Prints {2, 3}
+
+a = {1, 2, 3}
+b = {2}
+a.difference_update(b)  # Prints {1, 3}
---
+++ Strings_Lists_And_Slicing/speed_comparision_list_tuple_set.py
@@ -0,0 +1,47 @@
+import time
+
+
+def _time(func):
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        func(*args, **kwargs)
+        end_time = time.time()
+        print('Execution Time ',func.__name__, end_time-start_time)
+    return wrapper
+
+
+@_time
+def test_list():
+    l = []
+    for i in range(50000):
+        l.append(i)
+
+
+@_time
+def test_tuple():
+    t = tuple()
+    for i in range(50000):
+        t += (i,)
+
+
+@_time
+def test_set():
+    s = set()
+    for i in range(50000):
+        s.add(i)
+
+
+# Speed Test - Membership
+@_time
+def check_membership_list(item, _list):
+    print(item in _list)
+
+
+@_time
+def check_membership_tuple(item, _tuple):
+    print(item in _tuple)
+
+
+@_time
+def check_membership_set(item, _set):
+    print(item in _set)
