7dc1b0a618c6aad423c4ec4124e337cb2ae4e514
Sandeep <sandeep@Sandeeps-MacBook-Pro.local>
2020-12-16 22:08:40 +0530

    testing

--- 11_RegularExpressions/_regular_expressions.py
+++ 11_RegularExpressions/_regular_expressions.py
@@ -51,6 +51,117 @@ def search_pattern(_search_pattern, _search_string):
 greeting = "Hello world welcome to regular expressions in python"
 matches = re.findall("python", greeting)
 
+re.findall(r'hello', "hello world")
+
+re.findall('hello', "Hello world", re.IGNORECASE)
+
+re.findall(r'.', "hello world")
+
+re.findall(r'h.', "hello")
+
+re.findall(r'a.b', "acb")
+
+re.findall(r'a.b', "a b")
+
+re.findall(r"^hello", "hello world")
+
+re.findall(r"hello$", "world hello")
+
+re.findall(r'a.a', "ana")
+
+re.findall(r'a..a', "anna")
+
+re.findall(r'a.*a', "annnnnna")
+
+re.findall(r'a.*a', 'aa')
+
+re.findall(r"^a.*a$", "anna")
+
+re.findall(r"^a.*a$", "hello anna")
+
+re.findall(r"an*a", "hello ana")
+
+re.findall(r"an*a", "hello aa")
+
+re.findall(r"an*a", "hello annna")
+
+re.findall(r"an*a", "amma")
+
+re.findall(r'a.*a', 'abcad')
+
+re.findall(r'a.*a$', 'abcad')
+
+re.findall(r'a.*a$', 'abcada')
+
+re.findall(r'a.+a', 'ana')
+
+re.findall(r'a.+a', 'aa')
+
+re.findall(r'an?a', "ana")
+
+re.findall(r'an?a', "anna")
+
+re.findall(r'colou?r', "colour")
+
+re.findall(r'colou?r', "what color do you like")
+
+re.findall(r"[aeiou]", "hello")
+
+re.findall(r'[0-9]', 'The cost of the book is Rs.100')
+
+re.findall(r'[0-9]+', 'The cost of the book is Rs.100')
+
+re.findall(r"\d", 'The cost of the book is Rs.100')
+
+re.findall(r"\d+", 'The cost of the book is Rs.100')
+
+re.findall(r'[abcd]', 'hello world')
+
+re.findall(r'[abcd]', 'abcdefghijk')
+
+re.findall(r'[abcd]+', 'abcdefghijkab')
+
+re.findall(r'[^0-9]', 'Rs.100')
+
+re.findall(r'\D', 'Rs.100')
+
+re.findall(r"\w", "hello")
+
+re.findall(r"\w+", "hello")
+
+re.findall(r"\W", "hello world welcome to Python")
+
+re.findall(r'\w+', 'hello there')
+
+re.findall(r'\w+', 'hello_there')
+
+re.findall(r'\s', 'hello there    Hello')
+
+re.findall(r'\S', 'hello there    Hello')
+
+re.findall(r'\d\d\d\d\d\d', '560001')
+
+re.findall(r'\d{6}', '560001')
+
+re.findall(r'\w{3,5}', 'hi')
+
+re.findall(r'\w{3,5}', 'hello')
+
+re.findall(r'\w{3,5}', 'helloworld')
+
+re.findall(r'\w{3,5}', 'helloworld')
+
+re.findall(r'\w{3,}', 'hello')
+
+re.findall(r'\w{3,}', 'hi')
+
+re.findall(r'\w{1,}', 'hi')
+
+re.findall(r'\w{0,}', '')
+
+re.findall(r'^\w{0,1}$', 'hi')
+
+re.findall(r'^\w{0,1}$', 'h')
 
 # ===================================================
 # Regular expression - VOWELS
@@ -171,3 +282,16 @@ for url in urls:
     for match in matches:
         print(match)
 # ===================================================
+# Regular Expression - PAN Number
+PAN = ["ABCDE1234X", "ABCDE12341", "ABCE1234X"]
+pat = r'[A-Z]{5}[0-9]{4}[A-Z]'
+for pan in PAN:
+    match = search_pattern(pat, pan)
+    print(match)
+# ===================================================
+
+# Regular Expression - 24hr time format
+_formats = ['00:00:00', '23:59:59', '24:00:00', '1:59:20', '12:9:10', '10:20:8']
+for _format in _formats:
+    match = search_pattern(r"(?:[01]\d|2[0-3]):[0-5]\d:[0-5]\d", _format)
+    print(match)
