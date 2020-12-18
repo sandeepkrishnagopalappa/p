0d7b5a031a5c149a240207ee20ac959e0537f6e4
Sandeep <sandeep@Sandeeps-MacBook-Pro.local>
2020-12-18 12:41:11 +0530

    testing

--- 9_Generators_and_Iterator_Protocol/stocks_prices_live.py
+++ 9_Generators_and_Iterator_Protocol/stocks_prices_live.py
@@ -9,4 +9,4 @@ while True:
     for stock in stocks:
         data = nse.get_quote(stock)
         print(f"{stock:>8}, {data['lastPrice']:>10}, {data['dayHigh']:>10}, {data['pChange']:>10}")
-        time.sleep(0.2)
+        time.sleep(1)
