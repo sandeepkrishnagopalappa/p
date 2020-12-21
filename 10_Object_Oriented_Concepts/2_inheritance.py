8a51d5ab7b5a4e01508ce5649851550322ab9637
Sandeep <sandeep@Sandeeps-MacBook-Pro.local>
2020-12-21 09:35:01 +0530

    testing

--- 10_Object_Oriented_Concepts/2_inheritance.py
+++ 10_Object_Oriented_Concepts/2_inheritance.py
@@ -49,6 +49,38 @@ class Child5(Parent, Parent2):  # Child Inheriting from more than one parent
 c = Child5(10)
 print(c.__class__.__mro__)
 
+# Advanced Inheritance
+class Parent:
+    def spam(self):
+        print('Parent.Spam')
+
+
+class A(Parent):
+    def spam(self):
+        print('A.Spam')
+        super().spam()
+
+
+class B(A):
+    def spam(self):
+        print('B.Spam')
+        super().spam()
+
+
+class C(B):
+    def spam(self):
+        print('C.Spam')
+        super().spam()
+
+
+class D(Parent):
+    def spam(self):
+        print('D.Spam')
+        super().spam()
+
+
+class E(A, D):
+    pass
 
 # Calculating Payroll of different Employees
 class Employee:
