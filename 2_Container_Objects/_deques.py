f68e4c42dd5cad1765a168608cbabc2ca8f86540
Sandeep <sandeep@Sandeeps-MacBook-Pro.local>
2020-12-29 19:52:59 +0530

    added deque

---
+++ 2_Container_Objects/_deques.py
@@ -0,0 +1,21 @@
+from collections import deque
+
+# Make a new deque object with a string
+d = deque("hello")  # deque(['h', 'e', 'l', 'l', 'o'])
+
+# add a new entry to the right side
+d.append('w')   # deque(['h', 'e', 'l', 'l', 'o', 'w'])
+
+# add a new entry to the left side
+d.append('P')   # deque(['P', 'h', 'e', 'l', 'l', 'o', 'w'])
+
+# return and remove the rightmost item
+d.pop()     # deque(['P', 'h', 'e', 'l', 'l', 'o'])
+
+# return and remove the leftmost item
+d.popleft()     # deque(['h', 'e', 'l', 'l', 'o'])
+
+
+# To Be Continued....
+
+
