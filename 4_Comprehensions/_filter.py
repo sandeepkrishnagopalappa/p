fd8f3741f0a6df90c772075389589aff0849aea8
Sandeep <sandeep@Sandeeps-MacBook-Pro.local>
2021-03-12 09:49:57 +0530

    testing

--- 4_Comprehensions/_filter.py
+++ 4_Comprehensions/_filter.py
@@ -8,9 +8,20 @@ print(list(f))  # [2, 4, 6, 8]
 # Build a list with only even with even length string using filter func
 names = ['apple', 'google', 'yahoo', 'facebook', 'yelp', 'flipkart', 'gmail', 'instagram', 'microsoft']
 
+# Returns the string if the string has even number of characters.
 def even_len(item):
     if item % 2 == 0:
         return item
 
 f = filter(even_len, names)
 print(list(f))  # ['google', 'facebook', 'yelp', 'flipkart']
+
+
+names = ['laura', 'steve', 'bill', 'james', 'bob', 'greig', 'scott', 'alex', 'ive']
+# Returns the string if the string is starting from vowel character
+def get_vowels(name):
+    if name[0] in 'aeiou':
+        return name
+
+vowels = filter(get_vowels, names)
+print(list(vowels))
